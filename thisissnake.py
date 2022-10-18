ec855e48fa5612c78eaf7849f10a29c4c788e4f1
Tomas Schraal <tomas.schraal@gmail.com>
2022-10-18 12:00:51 +0200

    snake kleur, extra variabelen en ander spul

--- thisissnake.py
+++ thisissnake.py
@@ -1,13 +1,30 @@
+
 import pygame
+import time
+import random 
+
+snake_snelheid = 10
+
+window_x = 720
+window_y = 480
+
+zwart = pygame.Color(0, 0, 0)
+wit = pygame.Color(255, 255, 255)
+rood = pygame.Color(255, 0, 0)
+groen = pygame.Color(0, 255, 0)
+blauw = pygame.Color(0, 0, 255)
+
 pygame.init()
-dis=pygame.display.set_mode((800,600))
+window = pygame.display.set_mode((window_x,window_y)) #window grootte
 pygame.display.update()
-pygame.display.set_caption('SNOK')
+pygame.display.set_caption('SNOK') # window naam
 game_over=False
 while not game_over:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             game_over=True
  
+frames = pygame.time.Clock() # fps
+
 pygame.quit()
 quit()
